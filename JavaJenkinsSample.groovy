5aac6a2cc387e4d1cb652c117fc27cf737c56dbb
teodoragonovska <61463400+teodoragonovska@users.noreply.github.com>
2023-08-21 19:01:08 +0200

    Create JavaJenkinsSample.groovy

---
+++ JavaJenkinsSample.groovy
@@ -0,0 +1,55 @@
+pipeline {
+    agent any
+    stages {
+        stage('Clone') {
+            steps {
+                // Specify the directory where the repository should be checked out
+                dir('src/main/java/com/example/calculator') {
+                    git url: 'https://github.com/tgrip/rest-calculator.git', branch: 'master'
+                }
+            }
+        }
+        stage('Build') {
+            steps {
+                dir('src/main/java/com/example/calculator') {
+                    script {
+                        // Adjust the script based on the OS
+                        if (isUnix()) {
+                            sh './mvnw clean package'
+                        } else {
+                            bat '.\\mvnw clean package'
+                        }
+                    }
+                }
+            }
+        }
+        stage('Test') {
+            steps {
+                dir('src/main/java/com/example/calculator') {
+                    script {
+                        // Adjust the script based on the OS
+                        if (isUnix()) {
+                            sh './mvnw test'
+                        } else {
+                            bat '.\\mvnw test'
+                        }
+                    }
+                }
+            }
+        }
+        stage('Run') {
+            steps {
+                dir('src/main/java/com/example/calculator') {
+                    script {
+                        // Adjust the script based on the OS
+                        if (isUnix()) {
+                            sh './mvnw spring-boot:run -Dserver.port=8090'
+                        } else {
+                            bat '.\\mvnw spring-boot:run -Dserver.port=8090'
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
